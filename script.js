7f8fdcef7844be5bbc745d7f4cf17cc338f4982e
Luis Garza <luisgarzag27@gmail.com>
2024-08-30 17:06:49 -0600

    Finished the program: created playGame function and corrected minor errors

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 // Rock, paper, scissors
 // Luis Garza Gómez
 
+let humanScore, computerScore;
+humanScore = 0;
+computerScore = 0;
+
+playGame();
+
+function playGame() {
+    let humanChoice, computerChoice;
+    for (let i = 1; i <= 5; i++) {
+        computerChoice = getComputerChoice();
+        humanChoice = getHumanChoice();
+        console.log("Round " + i);
+        playRound(humanChoice, computerChoice);
+    }
+
+    if(computerScore > humanScore) {
+        console.log("Computer wins the game!!!");
+    }
+    else if(humanScore > computerScore) {
+        console.log("Congratulations. You win the game!!!");
+    }
+    else {
+        console.log("It's a Draw!!!");
+    }
+}
 // Function to get the computer's choice
-function getCumputerChoice() {
+function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
-
 // Function to get the human's choice
 function getHumanChoice() {
     let strOption = prompt("Choose: Rock, Paper, or Scissors");
@@ -31,33 +55,19 @@ function playRound(humanChoice, computerChoice) {
     if(humanChoice == computerChoice) {
         humanScore++;
         computerScore++;
-        console.log("Draw!");
+        console.log("Draw!\nYou: " + humanScore + " Computer: " + computerScore);
     }
     else if((humanChoice == 0 && computerChoice == 2) || 
     (humanChoice == 1 && computerChoice == 0) || 
     (humanChoice == 2 && computerChoice == 1)) {
         humanScore++;
-        console.log("You win the round!");
+        console.log("You win the round!\nYou: " + humanScore + " Computer: " + computerScore);
     }
     else if((humanChoice == 0 && computerChoice == 1) ||
     (humanChoice == 1 && computerChoice == 2) ||
     (humanChoice == 2 && computerChoice == 0)) {
         computerScore++;
-        console.log("The computer wins the round!");
+        console.log("The computer wins the round!\nYou: " + humanScore + " Computer: " + computerScore);
     }
 
-}
-
-function playGame() {
-    let humanScore, computerScore;
-    humanScore = 0;
-    computerScore = 0;
-
-    let humanChoice, computerChoice;
-    for (let i = 0; i < 5; index++) {
-        humanChoice = getCumputerChoice();
-        computerChoice = getHumanChoice();
-        playRound(humanChoice, computerChoice);
-    }
-    
 }
