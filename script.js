21be91d8458a7802cd0a0f8b706f1772f0628442
Luis Garza <luisgarzag27@gmail.com>
2024-09-10 23:52:10 -0600

    Added an id to the h2 and wrote the code that displays who wins the round and the game

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ round = 1;
 
 const humanScoreDisplay = document.querySelector("#humanScore");
 const computerScoreDisplay = document.querySelector("#computerScore");
+const h2Message = document.querySelector("#h2Message");
 
 function playGame(humanChoice) {
     let computerChoice;
     if (round < 5) {
+        h2Message.textContent = "";
         computerChoice = getComputerChoice();
         //console.log("Round " + round);
         playRound(humanChoice, computerChoice);
@@ -21,14 +23,19 @@ function playGame(humanChoice) {
         computerChoice = getComputerChoice();
         //console.log("Round " + round);
         playRound(humanChoice, computerChoice);
+        h2Message.style.cssText = "color: yellow";
         if(computerScore > humanScore) {
             console.log("Computer wins the game!!!");
+            h2Message.textContent = "Computer wins the game!!!";
         }
         else if(humanScore > computerScore) {
             console.log("Congratulations. You win the game!!!");
+            h2Message.textContent = "Congratulations. You win the game!!!";
+            
         }
         else {
             console.log("It's a Draw!!!");
+            h2Message.textContent = "It's a Draw!!!";
         }
         humanScore = 0;
         computerScore = 0;
@@ -41,10 +48,12 @@ function getComputerChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
+    h2Message.style.cssText = "color: black";
     if(humanChoice == computerChoice) {
         humanScore++;
         computerScore++;
 
+        h2Message.textContent = "Round " + round + ": Draw!";
         console.log("Draw!\nYou: " + humanScore + " Computer: " + computerScore);
         humanScoreDisplay.textContent = "Score " + humanScore;
         computerScoreDisplay.textContent = "Score " + computerScore;
@@ -53,6 +62,8 @@ function playRound(humanChoice, computerChoice) {
     (humanChoice == 1 && computerChoice == 0) || 
     (humanChoice == 2 && computerChoice == 1)) {
         humanScore++;
+
+        h2Message.textContent = "Round " + round + ": You win!";
         console.log("You win the round!\nYou: " + humanScore + " Computer: " + computerScore);
         humanScoreDisplay.textContent = "Score " + humanScore;
         computerScoreDisplay.textContent = "Score " + computerScore;
@@ -61,6 +72,8 @@ function playRound(humanChoice, computerChoice) {
     (humanChoice == 1 && computerChoice == 2) ||
     (humanChoice == 2 && computerChoice == 0)) {
         computerScore++;
+
+        h2Message.textContent = "Round " + round + ": The computer wins!";
         console.log("The computer wins the round!\nYou: " + humanScore + " Computer: " + computerScore);
         humanScoreDisplay.textContent = "Score " + humanScore;
         computerScoreDisplay.textContent = "Score " + computerScore;
